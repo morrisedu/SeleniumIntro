6eccdb25b2f5b6c189e973071df16d9ed9483b47
edumorris <88796134+morrisedu@users.noreply.github.com>
2021-09-28 12:38:28 +0300

    JavascriptExecutor with switching with WindowHandle

--- src/test/java/com/learn/HowTo/ExecuteJavaScriptCode.java
+++ src/test/java/com/learn/HowTo/ExecuteJavaScriptCode.java
@@ -2,16 +2,21 @@ package com.learn.HowTo;
 
 import com.cybertek.utilities.TestBase;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import java.util.Set;
 
 import static com.cybertek.utilities.BrowserUtil.waitFor;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExecuteJavaScriptCode extends TestBase {
     @Test
     public void testJavaScriptScroll() {
         driver.get("http://practice.cybertekschool.com/infinite_scroll");
 
-        // Cast driver to Javascript executor
+        // Cast driver to JavascriptExecutor
         JavascriptExecutor jse = (JavascriptExecutor) driver;
 
         // Scroll page using JavaScript
@@ -19,5 +24,47 @@ public class ExecuteJavaScriptCode extends TestBase {
             jse.executeScript("window.scrollBy(0, 1000)");
             waitFor(1);
         }
+    }
+
+    @Test
+    public void test_ScrollElementIntoView() {
+        driver.get("http://practice.cybertekschool.com/large");
+
+        WebElement school_link = driver.findElement(By.linkText("Cybertek School"));
+        WebElement home_link = driver.findElement(By.linkText("Home"));
+
+        // Cast driver to JavascriptExecutor
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+
+        // Execute JavaScript
+        jse.executeScript("arguments[0].scrollIntoView(true)", school_link);
+        waitFor(3);
+        jse.executeScript("arguments[0].scrollIntoView(true)", home_link);
+        waitFor(3);
+    }
+
+    @Test
+    public void testOpenNewTabAndNavigateToAUrl() {
+        driver.get("http://practice.cybertekschool.com");
+        waitFor(2);
+
+        // Cast driver to JavascriptExecutor
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+
+        // Execute JavaScript
+        jse.executeScript("window.open('https://google.com', '_blank')");
+        waitFor(2);
+
+        // Switch to new tab
+        Set<String> all_tabs = driver.getWindowHandles();
+
+        for (String tab : all_tabs) {
+            driver.switchTo().window(tab);
+        }
+
+        assertEquals("Google", driver.getTitle());
+
+        // Switch to the new tab
+
     }
 }
